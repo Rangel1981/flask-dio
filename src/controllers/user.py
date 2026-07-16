7bd3a3aa5ff2094d86c40ba6e75e5024209b0666
Arthur <aarthur.rangel98@gamil.com>
2026-07-16 11:29:04 -0300

    Criando a rota delete

---
+++ src/controllers/user.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, request
+from src.main import db, User
+from http import HTTPStatus
+from sqlalchemy import inspect
+
+
+bp = Blueprint("user", __name__, url_prefix="/users")
+
+
+def _create_user():
+    data = request.json
+    user = User(username=data["username"])
+    db.session.add(user)
+    db.session.commit()
+   
+def _list_useres():
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [{"id": user.id, "username": user.username} for user in users]
+
+
+@bp.route("/", methods=["GET" , "POST"])
+def handler_users():
+    if request.method == "POST":
+        _create_user()
+        return {"message": "User created successfully"}, HTTPStatus.CREATED
+    else:
+        return {"users": _list_useres()}, HTTPStatus.OK
+
+@bp.route("/<int:user_id>")    
+def _get_user(user_id: int):
+    user = db.get_or_404(User, user_id)
+    return {"id": user.id, "username": user.username}
+
+@bp.route("/<int:user_id>", methods=["PATCH"])    
+def update_user(user_id: int):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+   
+    mapper = inspect(User)
+    for columnn in mapper.attrs:
+        if columnn.key in data:
+            setattr(user, columnn.key, data[columnn.key])
+        db.session.commit()
+
+
+    return {"id": user.id, "username": user.username}
+
+
+@bp.route("/<int:user_id>", methods=["DELETE"] )    
+def delete_user(user_id: int):
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return {"message": "User deleted successfully"}
---
+++ src/main.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+import os
+import click
+
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+import sqlalchemy as sa
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String(80), unique=True)
+
+    def __repr__(self) -> str:
+        return f'User(id={self.id!r}, username={self.username!r})'
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    title: Mapped[str] = mapped_column(sa.String(100), nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    author_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})'
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+
+    click.echo('Initialized the database.')
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI= 'sqlite:///db.sqlite'
+        )
+    
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+   
+       
+    app.cli.add_command(init_db_command)
+    db.init_app(app)
+    
+    #resgister blueprints
+    from src.controllers import user
+    app.register_blueprint(user.bp)
+
+    return app
